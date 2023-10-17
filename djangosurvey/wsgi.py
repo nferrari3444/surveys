8f6aee4072465c38dfc1b08a5343bf5803f83134
Nicolas Ferrari <nicolas.ferrari@pedidosya.com>
2023-10-17 18:16:18 -0300

    remove base in settings

--- djangosurvey/wsgi.py
+++ djangosurvey/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosurvey.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosurvey.settings")
 
 application = get_wsgi_application()
 
