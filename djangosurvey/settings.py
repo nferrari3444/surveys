a38e11ffa598bfacf9e45db9d5043f0b36962dba
Nicolas-at-675755258015 <nferrari3444@gmail.com>
2024-02-19 17:46:20 -0300

    add environment variables

--- djangosurvey/settings.py
+++ djangosurvey/settings.py
@@ -194,10 +194,23 @@ INTERNAL_IPS = [
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': env('DATABASE_URL'),
+        'NAME': env('PGDATABASE'),
+        'USER': 'postgres',
+        'PASSWORD': env('PGPASSWORD'),
+         'HOST': env('PGHOST'),
+         'PORT': 36295,
     }
 }
 
