01ed9565e7b52d13bd316b28e62091c88d502044
nferrari3444 <nferrari3444@gmail.com>
2026-03-08 17:26:49 -0300

    Change wsgi.py file to load settings associated with the current environment in which the application run

--- djangosurvey/wsgi.py
+++ djangosurvey/wsgi.py
@@ -8,10 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from djangosurvey.settings import base
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosurvey.settings")
+if base.DEBUG == True:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings.dev')
+    
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings.prod')
+
+
 
 application = get_wsgi_application()
 
